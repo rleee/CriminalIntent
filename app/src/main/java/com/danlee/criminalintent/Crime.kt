9fe8133121adf46b56616f1f0c9d46a7edde7782
rleee <danlee0150@gmail.com>
2020-03-03 19:16:40 +0800

    Finish chapter 8, Fragment

---
+++ app/src/main/java/com/danlee/criminalintent/Crime.kt
@@ -0,0 +1,9 @@
+package com.danlee.criminalintent
+
+import java.util.*
+
+data class Crime(val id: UUID = UUID.randomUUID(),
+                 var title: String = "",
+                 var date: Date = Date(),
+                 var isSolved: Boolean = false) {
+}
