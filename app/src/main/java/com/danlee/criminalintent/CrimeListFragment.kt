e36072275f36e6583038a61b15753cbbbeae5ff1
rleee <danlee0150@gmail.com>
2020-03-04 14:55:39 +0800

    Finish chapter 10, ConstraintLayout

--- app/src/main/java/com/danlee/criminalintent/CrimeListFragment.kt
+++ app/src/main/java/com/danlee/criminalintent/CrimeListFragment.kt
@@ -5,6 +5,7 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
@@ -61,6 +62,7 @@ class CrimeListFragment: Fragment() {
 
         private val titleTextView: TextView = view.findViewById(R.id.crime_title)
         private val dateTextView: TextView = view.findViewById(R.id.crime_date)
+        private val solvedImageView: ImageView = view.findViewById(R.id.crime_solved)
 
         init {
             view.setOnClickListener(this)
@@ -70,6 +72,11 @@ class CrimeListFragment: Fragment() {
             this.crime = crime
             this.titleTextView.text = this.crime.title
             this.dateTextView.text = this.crime.date.toString()
+            solvedImageView.visibility = if (crime.isSolved) {
+                View.VISIBLE
+            } else {
+                View.GONE
+            }
         }
 
         override fun onClick(v: View?) {
